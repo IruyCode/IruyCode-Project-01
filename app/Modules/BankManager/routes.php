602c2c13e4524acd8636696ed9e8d8da55a43683
IruyCode <vzx1red@gmail.com>
2025-11-06 17:43:28 +0000

    feat(bank): create initial structure for the BankManager module

---
+++ app/Modules/BankManager/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Modules\BankManager\Controllers\TransactionController;
+
+Route::prefix('bank')->name('bank.')->group(function () {
+
+});
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+foreach (glob(base_path('app/Modules/*/routes.php')) as $routeFile) {
+    require $routeFile;
+}
+
+
 Route::get('/', function () {
     return view('welcome');
 });
